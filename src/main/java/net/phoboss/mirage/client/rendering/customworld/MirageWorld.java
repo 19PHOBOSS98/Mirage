1df5d6f8ea665df2689df9730f2e476c3d7d3319
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-02-12 16:00:31 +0800

    clear mirageStateNEntities after initBlockRenderLists

--- src/main/java/net/phoboss/mirage/client/rendering/customworld/MirageWorld.java
+++ src/main/java/net/phoboss/mirage/client/rendering/customworld/MirageWorld.java
@@ -447,7 +447,7 @@ public class MirageWorld extends Level implements ServerLevelAccessor {
 
             this.vertexBufferBlocksList.put(blockPosKey,stateNEntity);
         });
-
+        this.mirageStateNEntities.clear();
     }
 
     public static void renderMirageBlockEntity(BlockEntity blockEntity, float tickDelta, PoseStack matrices, MultiBufferSource vertexConsumers){
