e8871ec0fa6f97008c4e4019b0622b48ae078693
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-02-12 18:44:07 +0800

    update tooltips

--- src/main/java/net/phoboss/mirage/items/mirageprojector/MirageBlockItem.java
+++ src/main/java/net/phoboss/mirage/items/mirageprojector/MirageBlockItem.java
@@ -1,8 +1,14 @@
 package net.phoboss.mirage.items.mirageprojector;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
 import net.phoboss.mirage.blocks.mirageprojector.MirageBlockEntity;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -12,6 +18,8 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.util.GeckoLibUtil;
+
+import java.util.List;
 
 public class MirageBlockItem extends BlockItem implements IAnimatable {
     public MirageBlockItem(Block block, Settings settings) {
@@ -28,6 +36,22 @@ public class MirageBlockItem extends BlockItem implements IAnimatable {
     private PlayState predicate(AnimationEvent<MirageBlockEntity> event) {
         event.getController().setAnimation(new AnimationBuilder().addAnimation("projecting", ILoopType.EDefaultLoopTypes.LOOP));
         return PlayState.CONTINUE;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("block.mirage.item.tooltip.shift_0"));
+            tooltip.add(Text.translatable("block.mirage.item.tooltip.shift_1"));
+            tooltip.add(Text.translatable("block.mirage.item.tooltip.shift_2"));
+            tooltip.add(Text.translatable("block.mirage.item.tooltip.shift_3"));
+        } else {
+            tooltip.add(Text.translatable("block.mirage.item.tooltip_0"));
+            tooltip.add(Text.translatable("block.mirage.item.tooltip_1"));
+            tooltip.add(Text.translatable("block.mirage.item.tooltip_2"));
+            tooltip.add(Text.translatable("block.mirage.item.tooltip_3"));
+            tooltip.add(Text.translatable("block.mirage.item.tooltip_4"));
+        }
     }
 
     @Override
