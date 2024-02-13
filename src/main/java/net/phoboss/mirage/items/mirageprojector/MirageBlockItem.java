42c8424c831e8b5b0dfc5ddafc3d2ae823de1987
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-02-13 08:38:51 +0800

    updated tooltips, added Book Settings Guide

--- src/main/java/net/phoboss/mirage/items/mirageprojector/MirageBlockItem.java
+++ src/main/java/net/phoboss/mirage/items/mirageprojector/MirageBlockItem.java
@@ -1,11 +1,18 @@
 package net.phoboss.mirage.items.mirageprojector;
 
 
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.IItemRenderProperties;
 import net.phoboss.mirage.blocks.mirageprojector.MirageBlockEntity;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -16,11 +23,49 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class MirageBlockItem extends BlockItem implements IAnimatable {
     public MirageBlockItem(Block block, Properties settings) {
         super(block, settings);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pFlag) {
+        if (Screen.hasShiftDown()) {
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.shift.0"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.shift.1"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.shift.2"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.shift.3"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.shift.4"));
+        } else if(Screen.hasControlDown()){
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.0"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.1"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.2"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.3"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.4"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.5"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.6"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.7"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.8"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.9"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.10"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.11"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.12"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.13"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.14"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.ctrl.15"));
+        }
+        else {
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.0"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.1"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.2"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.3"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.4"));
+            tooltip.add(new TranslatableComponent("block.mirage.item.tooltip.5"));
+        }
+        super.appendHoverText(pStack, pLevel, tooltip, pFlag);
     }
 
     public AnimationFactory factory = GeckoLibUtil.createFactory(this);
