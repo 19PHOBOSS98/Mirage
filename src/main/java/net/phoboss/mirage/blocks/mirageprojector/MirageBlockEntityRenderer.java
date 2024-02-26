0f103b6d64461a2b161de7884435d24bcefedbe8
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-02-26 18:59:31 +0800

    forgot to change the BER render method for the new book settings

--- src/main/java/net/phoboss/mirage/blocks/mirageprojector/MirageBlockEntityRenderer.java
+++ src/main/java/net/phoboss/mirage/blocks/mirageprojector/MirageBlockEntityRenderer.java
@@ -8,12 +8,16 @@ import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RotationAxis;
 import net.minecraft.util.math.Vec3d;
 import net.phoboss.mirage.client.rendering.customworld.MirageWorld;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -24,8 +28,8 @@ public class MirageBlockEntityRenderer extends GeoBlockRenderer<MirageBlockEntit
     }
 
     @Override
-    public void renderFinal(MatrixStack poseStack, MirageBlockEntity blockEntity, BakedGeoModel model, VertexConsumerProvider bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        super.renderFinal(poseStack, blockEntity, model, bufferSource, buffer, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
+    public void renderFinal(MatrixStack poseStack, MirageBlockEntity blockEntity, BakedGeoModel model, VertexConsumerProvider bufferSource, VertexConsumer buffer, float tickDelta, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        super.renderFinal(poseStack, blockEntity, model, bufferSource, buffer, tickDelta, packedLight, packedOverlay, red, green, blue, alpha);
 
         if(!blockEntity.isPowered()) {
             return;
@@ -42,11 +46,70 @@ public class MirageBlockEntityRenderer extends GeoBlockRenderer<MirageBlockEntit
         MirageWorld mirageWorld = mirageWorldList.get(mirageWorldIndex);
 
         if (mirageWorld != null) {
+            long pGameTime = blockEntity.getWorld().getTime();
+            float time = pGameTime + tickDelta;
+            //float time = (float)pGameTime + tickDelta;
+
             BlockPos projectorPos = blockEntity.getPos();
-            //poseStack.pushPose();//TODO: add this as book settings
-            //poseStack.mulPose(new Quaternionf(new Vector3f(0,0,1),45,true));
-            mirageWorld.render(projectorPos, partialTick, poseStack, bufferSource, packedLight, 0);
-            //poseStack.popPose();
+
+            MirageProjectorBook bookSettings = blockEntity.getBookSettingsPOJO();
+
+            float[] pScale = bookSettings.getPScale();
+            float[] pMove = bookSettings.getPMove();
+            Quaternionf pRotate = bookSettings.getPRotateAsQuat();
+            float[] pRotatePivot = bookSettings.getPRotatePivot();
+            float[] pSpinPivot = bookSettings.getPSpinPivot();
+            Vector3f pSpinAxis = bookSettings.getPSpinAxisAsVec3();
+            float pSpinSpeed = bookSettings.getPSpinSpeed();
+            float pSpinOffset = bookSettings.getPSpinOffset();
+            HashMap<Integer,Frame> frames = blockEntity.getBookSettingsPOJO().getFrames();
+            Frame mwFrame = frames.get(mirageWorldIndex);
+
+            poseStack.push();
+
+            poseStack.scale(pScale[0],pScale[1],pScale[2]);
+
+            if(mwFrame != null) {
+                float[] pScaleFrame = mwFrame.getPScale();
+                poseStack.scale(pScaleFrame[0],pScaleFrame[1],pScaleFrame[2]);
+            }
+
+            poseStack.push();
+            poseStack.translate(pMove[0],pMove[1],pMove[2]);
+
+            if(mwFrame != null){
+                float[] pMoveFrame = mwFrame.getPMove();
+                poseStack.translate(pMoveFrame[0],pMoveFrame[1],pMoveFrame[2]);
+            }
+
+            poseStack.translate(pSpinPivot[0],pSpinPivot[1],pSpinPivot[2]);
+            poseStack.multiply(RotationAxis.of(pSpinAxis).rotationDegrees(time * pSpinSpeed * 0.05F - pSpinOffset)); // derived from BeaconBlockEntityRenderer: 45 deg/sec:2.25F (2.25/45=0.05) didn't bother to look into it more :)
+            poseStack.translate(-pSpinPivot[0],-pSpinPivot[1],-pSpinPivot[2]);
+
+            if(mwFrame != null) {
+                float[] pSpinPivotFrame = mwFrame.getPSpinPivot();
+                Vector3f pSpinAxisFrame = mwFrame.getPSpinAxisAsVec3();
+                float pSpinSpeedFrame = mwFrame.getPSpinSpeed();
+                float pSpinOffsetFrame = mwFrame.getPSpinOffset();
+                poseStack.translate(pSpinPivotFrame[0],pSpinPivotFrame[1],pSpinPivotFrame[2]);
+                poseStack.multiply(RotationAxis.of(pSpinAxisFrame).rotationDegrees(time * pSpinSpeedFrame * 0.05F - pSpinOffsetFrame));
+                poseStack.translate(-pSpinPivotFrame[0],-pSpinPivotFrame[1],-pSpinPivotFrame[2]);
+            }
+
+            poseStack.translate(pRotatePivot[0],pRotatePivot[1],pRotatePivot[2]);
+            poseStack.multiply(pRotate);
+            poseStack.translate(-pRotatePivot[0],-pRotatePivot[1],-pRotatePivot[2]);
+
+            if(mwFrame != null) {
+                float[] pRotatePivotFrame = mwFrame.getPRotatePivot();
+                poseStack.translate(pRotatePivotFrame[0],pRotatePivotFrame[1],pRotatePivotFrame[2]);
+                poseStack.multiply(mwFrame.getPRotateAsQuat());
+                poseStack.translate(-pRotatePivotFrame[0],-pRotatePivotFrame[1],-pRotatePivotFrame[2]);
+            }
+            mirageWorld.render(projectorPos, tickDelta, poseStack, bufferSource, packedLight, 0);
+
+            poseStack.pop();
+            poseStack.pop();
         }
     }
 
