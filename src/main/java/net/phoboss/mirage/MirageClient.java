ec4419d0d8d226f0a04c070d34fa5f7d8fc4ae1c
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-02-29 13:44:50 +0800

    implemented threadpool. Mirage Projectors are now queued to use a maximum of 2 MirageLoader threads

--- src/main/java/net/phoboss/mirage/MirageClient.java
+++ src/main/java/net/phoboss/mirage/MirageClient.java
@@ -3,12 +3,11 @@ package net.phoboss.mirage;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientLoginConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientLoginNetworkHandler;
 import net.phoboss.mirage.client.rendering.ModRendering;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+
+import java.util.concurrent.Executors;
 
 @Environment(EnvType.CLIENT)
 public class MirageClient implements ClientModInitializer {
@@ -17,14 +16,13 @@ public class MirageClient implements ClientModInitializer {
         ModRendering.registerAll();
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             System.gc();
+            Mirage.THREAD_POOL = Executors.newFixedThreadPool(2,new BasicThreadFactory.Builder()
+                    .namingPattern("MirageLoader-%d")
+                    .priority(Thread.MAX_PRIORITY)
+                    .build());
         });
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
-            for(Thread thread:Thread.getAllStackTraces().keySet()){
-                if(thread.getName().equals("MirageLoader") && !thread.isInterrupted()){
-                    thread.interrupt();
-                }
-            }
+            Mirage.THREAD_POOL.shutdownNow();
         });
     }
-
 }
